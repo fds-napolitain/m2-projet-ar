de98e10cb95d829c80a83100ee0eb07c70817de8
Yann <srenjzzz@gmail.com>
2021-11-26 12:54:37 +0100

    ptit update

--- Assets/PianoToucheScript.cs
+++ Assets/PianoToucheScript.cs
@@ -8,11 +8,12 @@
 
 using Leap.Unity;
 using Leap.Unity.Query;
+using Leap;
 using Leap.Unity.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 
 public class PianoToucheScript : MonoBehaviour
 {
@@ -22,9 +23,18 @@ public class PianoToucheScript : MonoBehaviour
     private InteractionBehaviour _intObj;
     private bool keySuspended = true;
     private float _fingertipRadius = 0.01f; // 1 cm
+    private static Controller controller;
+    private static Frame frame;
+    private static bool flag = false; 
 
     void Start()
     {
+
+        if (!flag)
+        {
+            controller = new Controller();
+            flag = true;
+        }
         /*
         _intObj = GetComponent<InteractionBehaviour>();
         _intObj.OnContactBegin += () =>
@@ -71,16 +81,38 @@ public class PianoToucheScript : MonoBehaviour
         {
             _material = renderer.material;
         }*/
-        
+
     }
 
-    void OnCollisionEnter(Collision Collision)
+    void OnTriggerEnter(Collider collider )
     {
-        GetComponent<AudioSource>().Play();
-        Debug.Log(Collision.contacts);
+
+
+        frame = controller.Frame();
+
+        int a = Convert.ToInt32(collider.name);
+        Debug.Log(frame.Hands.Count);
+        if (frame.Hands.Count >= 1 && !frame.Hands[0].IsLeft)
+        {
+            if (a > 4)
+            {
+                a -= 5;
+            }
+            else
+            {
+                a += 5;
+            }
+        } 
+        if (frame.Hands[a/5].Fingers[a%5].IsExtended)
+        {
+           GetComponent<AudioSource>().Play();
+        }
+
+
+
     }
-   
-    void OnCollisionExit()
+
+    void OnTriggerExit()
     {
         
     }
