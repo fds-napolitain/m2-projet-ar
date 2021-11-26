433cc491ae476acdd0b74932f877373a5a5b5b70
Maxime Boucher <mxboucher@gmail.com>
2021-11-26 14:58:09 +0100

    script release touche

--- Assets/PianoToucheScript.cs
+++ Assets/PianoToucheScript.cs
@@ -17,19 +17,23 @@ using System;
 
 public class PianoToucheScript : MonoBehaviour
 {
+    private static float QUANTIZATION = 0.25f;
+    private static float RELEASE_TIME = 0.33f;
+    private float release_tmp = 0;
+    private bool release = false;
+    private AudioSource audio;
 
+    private static Controller controller;
+    private static Frame frame;
+    private static bool flag = false;
     private Material _material;
-
     private InteractionBehaviour _intObj;
     private bool keySuspended = true;
     private float _fingertipRadius = 0.01f; // 1 cm
-    private static Controller controller;
-    private static Frame frame;
-    private static bool flag = false; 
 
     void Start()
     {
-
+        audio = GetComponent<AudioSource>(); // recupere l'audiosource dans une variable
         if (!flag)
         {
             controller = new Controller();
@@ -86,15 +90,12 @@ public class PianoToucheScript : MonoBehaviour
 
     void OnTriggerEnter(Collider collider )
     {
-
-
         frame = controller.Frame();
-
         int a = Convert.ToInt32(collider.name);
         Debug.Log(frame.Hands.Count);
-        if (frame.Hands.Count >= 1 && !frame.Hands[0].IsLeft)
+        if (frame.Hands.Count >= 1 && !frame.Hands[0].IsLeft) // si la main "gauche" est celle de "droite"
         {
-            if (a > 4)
+            if (a > 4) // inversement
             {
                 a -= 5;
             }
@@ -103,23 +104,32 @@ public class PianoToucheScript : MonoBehaviour
                 a += 5;
             }
         } 
-        if (frame.Hands[a/5].Fingers[a%5].IsExtended)
+        if (frame.Hands[a/5].Fingers[a%5].IsExtended) // joue le son
         {
-           GetComponent<AudioSource>().Play();
+           audio.Play();
         }
-
-
-
     }
 
     void OnTriggerExit()
     {
-        
+        release = true;
     }
 
     void Update()
     {
-
+        // release de la touche jusqu'a fin de note (release_time)
+        if (release)
+        {
+            release_tmp += Time.deltaTime;
+            audio.volume -= Time.deltaTime / RELEASE_TIME;
+            if (release_tmp >= RELEASE_TIME)
+            {
+                release_tmp = 0;
+                release = false;
+                audio.Stop();
+                audio.volume = 1f;
+            }
+        }
     }
 
 }
